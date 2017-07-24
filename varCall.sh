85f53d32e84b21be23e58273bc393edc2ed44f97
xuzhougeng <18868192325m0@sina.cn>
2017-07-24 17:53:00 +0800

    2017年 07月 24日 星期一 17:52:59 DST

--- varCall.sh
+++ varCall.sh
@@ -2,22 +2,30 @@
 # version 1.1
 # function:  provide different function to align and vairant calling
 # author: xuzhougeng, xuzhougeng@163.com
-# parameter: samplePath threads
+# parameter: samplePath, index, reference threads
 
 set -e
 set -u
 set -o pipefail
 
 PATH=/home/wangjw/miniconda3/bin:/usr/local/bin:/usr/bin:/bin
+echo "you should run this program in the project root path "
+if [ $# -lt 4 ]
+then
+  echo -e "$# less than 4, please enter samplePath, index, reference and threads"
+  exit 1
+fi
 
 # /public1/wangjw/linzhi_20170715/Data/PD/FCH2LCHCCXY_L7_wHAXPI051602-43
 samplePath=$1
-threads=$2
-index=/public1/wangjw/linzhi_20170715/chiIndex/hirsuta.fa
-reference=/public1/wangjw/linzhi_20170715/chiIndex/hirsuta.fa
+index=$2
+reference=$3
+threads=$4
+#index=/public1/wangjw/linzhi_20170715/chiIndex/hirsuta.fa
+#reference=/public1/wangjw/linzhi_20170715/chiIndex/hirsuta.fa
 
-mkdir -p /public1/wangjw/linzhi_20170715/alignment
-alignDir=/public1/wangjw/linzhi_20170715/alignment
+mkdir -p alignment
+alignDir=alignment
 # alignment
 for sample in `cat $samplePath`
 do
@@ -34,26 +42,26 @@ do
 done
 
 # convert sort and index
+# warning total memoery > threads x memeory
 
 for sample in `cat $samplePath`
 do
   echo "processing $filename with samtools"
   output=${sample##*/}
   samtools view -@ $threads -b -o $alignDir/${output}.bam $alignDir/${filename}.sam
-  samtools sort -@ $threads -m 8G -o $alignDir/${output}.sorted.bam $alignDir/${output}.bam
+  samtools sort -@ $threads -m 2G -o $alignDir/${output}.sorted.bam $alignDir/${output}.bam
   samtools index -@ $threads $alignDir/${output}.sorted.bam
   echo "$filename done"
 done
 
 
 # vairant calling with bcftools
-mkdir -p mkdir -p /public1/wangjw/linzhi_20170715/variant_bcftools
-varDir=/public1/wangjw/linzhi_20170715/variant_bcftools
+mkdir -p variant_bcftools
+varDir=variant_bcftools
 for sample in `cat $samplePath`
 do
   output=${sample##*/}
   echo "processing $sample with bcftools"
   samtools mpileup  -vu -t AD,DP -f $reference $alignDir/${output}.sorted.bam | \
-  bcftools call -vm -Ov > $varDir/${output%%.*}_raw_variants.vcf &
-  echo "$sample done "
+  bcftools call -vm -Ov > $varDir/${output%%.*}_raw_variants.vcf && echo "$sample done " &
 done
