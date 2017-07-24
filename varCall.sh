464c9badba8b7c5820e5188d8fd24bb4694c2eec
xuzhougeng <18868192325m0@sina.cn>
2017-07-24 10:01:40 +0800

    add new parameter

--- varCall.sh
+++ varCall.sh
@@ -2,6 +2,7 @@
 # version 1.1
 # function:  provide different function to align and vairant calling
 # author: xuzhougeng, xuzhougeng@163.com
+# parameter: samplePath threads
 
 set -e
 set -u
@@ -11,12 +12,12 @@ PATH=/home/wangjw/miniconda3/bin:/usr/local/bin:/usr/bin:/bin
 
 # /public1/wangjw/linzhi_20170715/Data/PD/FCH2LCHCCXY_L7_wHAXPI051602-43
 samplePath=$1
+threads=$2
 index=/public1/wangjw/linzhi_20170715/chiIndex/hirsuta.fa
 reference=/public1/wangjw/linzhi_20170715/chiIndex/hirsuta.fa
 
 mkdir -p /public1/wangjw/linzhi_20170715/alignment
 alignDir=/public1/wangjw/linzhi_20170715/alignment
-
 # alignment
 for sample in `cat $samplePath`
 do
@@ -32,9 +33,9 @@ for sample in `cat $samplePath`
 do
   echo "processing $filename with samtools"
   output=${sample##*/}
-  samtools view -b -o $alignDir/${output}.bam $alignDir/${filename}.sam
-  samtools sort -o $alignDir/${output}.sorted.bam $alignDir/${output}.bam
-  samtools index $alignDir/${output}.sorted.bam
+  samtools view -@ $threads -b -o $alignDir/${output}.bam $alignDir/${filename}.sam
+  samtools sort -@ $threads -m 8G -o $alignDir/${output}.sorted.bam $alignDir/${output}.bam
+  samtools index -@ $threads $alignDir/${output}.sorted.bam
   echo "$filename done"
 done
 
@@ -47,6 +48,6 @@ do
   output=${sample##*/}
   echo "processing $sample with bcftools"
   samtools mpileup  -vu -t AD,DP -f $reference $alignDir/${output}.sorted.bam | \
-  bcftools call -vm -Ov > $varDir/${output%%.*}_raw_variants.vcf
+  bcftools call -vm -Ov > $varDir/${output%%.*}_raw_variants.vcf &
   echo "$sample done "
 done
