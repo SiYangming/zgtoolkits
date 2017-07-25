4e060734944c2d3ebd34f4811f0dde4b72a7fc18
xuzhougeng <18868192325m0@sina.cn>
2017-07-25 21:30:20 +0800

    delete

--- runHawk.sh
+++
@@ -1,8 +0,0 @@
-hawkDir=/scratch/atif/hawk
-
-caseCount=$(cat case_sorted_files.txt | wc -l);
-controlCount=$(cat control_sorted_files.txt | wc -l);
-
-$hawkDir/hawk $caseCount $controlCount
-
-$hawkDir/bonf_fasta  
